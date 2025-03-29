e1211c0c456ade280b259a1d2f299d210b59ca16
Thomas Le Sciellour <thomas.lesciellour@observia-group.com>
2025-03-29 13:01:35 +0100

    add base curl command

---
+++ src/utils/commands/curl.ts
@@ -0,0 +1,19 @@
+import { CanvasContext } from '@/utils/CanvasContext'
+
+export default class {
+  static call = 'cat <url>'
+  static description = "Affiche le contenu d'une page web"
+
+  static async handle(context: CanvasContext, args: string[]) {
+    try {
+      const url = args[0]
+      const response = await fetch(url, { mode: 'no-cors' })
+      const text = await response.text()
+
+      context.println(text)
+    } catch (e) {
+      console.log(e)
+      context.println(e.message)
+    }
+  }
+}
